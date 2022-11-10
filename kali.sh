14f08841a1a4c6e343b7445fa1481e6ce5790c0a
SMN947 <53320639+SMN947@users.noreply.github.com>
2022-11-09 21:02:50 -0500

    Create kali.sh

---
+++ kali.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Actualizacion de paquetes
+sudo apt update && sudo apt upgrade
+
+#Intalacion de GUI
+sudo apt install -y kali-win-kex
+
+#Instalacion de ZSH
+sudo apt install zsh zsh-syntax-highlighting zsh-autosuggestions
