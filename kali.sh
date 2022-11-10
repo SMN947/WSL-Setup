fb4e1275cfd3febc37e77590d275ab3e0fad71d0
SMN947 <53320639+SMN947@users.noreply.github.com>
2022-11-09 21:49:11 -0500

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -8,3 +8,12 @@ sudo apt install -y kali-win-kex
 
 #Instalacion de ZSH
 sudo apt install zsh zsh-syntax-highlighting zsh-autosuggestions
+
+#Instalacion de git
+sudo apt install git
+
+#Instalacion de oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#Cambiar terminal
+chsh -s /bin/zsh smn947
